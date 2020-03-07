3f158e32fc0a99c6390d27bd0b5c4a985dbc99ef
Graeme Hosford <graeme.hosford@gmail.com>
2020-03-07 13:36:31 +0000

    Generated grpc code for tweet_reader.py to make it act as a server. Server seems to run fine but client gives crash when trying to connect, haven't figured out yet

--- tweet_analytics_client.py
+++ tweet_analytics_client.py
@@ -1,2 +1,20 @@
 # Name: Graeme Hosford
 # Student ID: R00147327
+
+import tweetreader_pb2
+import tweetreader_pb2_grpc
+
+import grpc
+
+
+def run():
+    while True:
+        with grpc.insecure_channel('tweet_reader:50051') as channel:
+            stub = tweetreader_pb2_grpc.TweetReaderStub(channel)
+            response = stub.getTweets(tweetreader_pb2.TweetRequest())
+            for item in response:
+                print(item.text)
+
+
+if __name__ == '__main__':
+    run()
--- tweet_reader.py
+++ tweet_reader.py
@@ -1,6 +1,20 @@
 from concurrent import futures
 import pandas as pd
 import grpc
+import time
+
+import tweetreader_pb2
+import tweetreader_pb2_grpc
+
+
+tweet_date = None
+
+
+class TweetReader(tweetreader_pb2_grpc.TweetReaderServicer):
+    def getTweets(self, request, context):
+        for tweets in tweet_date.iterrows():
+            time.sleep(0.5)
+            yield tweetreader_pb2.Tweet(target=tweets["target"], text=tweets["text"], username=tweets["user"])
 
 
 def read_in_tweets():
@@ -11,11 +25,15 @@ def read_in_tweets():
 
 
 def serve():
+    read_in_tweets()
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    server.add_insecure_port("{::}:50051")
+    tweetreader_pb2_grpc.add_TweetReaderServicer_to_server(TweetReader(), server)
+    server.add_insecure_port("[::]:50051")
     server.start()
+    print("Server started")
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    read_in_tweets()
+    serve()
